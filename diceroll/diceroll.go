7e4007a7c087fd81ae94fadc312a102ea49c9481
felipezoltowski <felipe.zoltowski@gmail.com>
2024-12-10 10:17:37 -0300

    fix hot reload

--- diceroll/diceroll.go
+++ diceroll/diceroll.go
@@ -5,7 +5,14 @@ const (
 	maxDiceValue int = 20
 )
 
-// Handle the logic for Natural One
+func calcDegreesThreshold(dc, offset int) int {
+	return dc + offset
+}
+
+func calcOddsRange(maxDiceValue, threshold, modifier, offset int) int {
+	return max(0, min(maxDiceValue, threshold-modifier+offset))
+}
+
 func HandleNaturalOne(
 	naturalOne, criticalFailureThreshold, failureThreshold, successThreshold, maxDiceValue int,
 	criticalFailures, failures, successes, criticalSuccesses *int,
@@ -27,7 +34,6 @@ func HandleNaturalOne(
 	}
 }
 
-// Handle the logic for Natural Twenty
 func HandleNaturalTwenty(
 	naturalTwenty, successThreshold, failureThreshold, maxDiceValue int,
 	criticalFailures, failures, successes, criticalSuccesses *int,
@@ -54,34 +60,28 @@ func HandleNaturalTwenty(
 	}
 }
 
-// calculate odds range
-func calculateRange(maxDiceValue, threshold, modifier, offset int) int {
-	return max(0, min(maxDiceValue, threshold-modifier+offset))
-}
-
 func DiceRollOdds(modifier, dc int) (criticalFailures, failures, successes, criticalSuccesses int) {
 
-	criticalFailureThreshold := dc - 10
-	failureThreshold := dc
-	successThreshold := dc + 10
+	criticalFailureThreshold := calcDegreesThreshold(dc, -10)
+	failureThreshold := calcDegreesThreshold(dc, 0)
+	successThreshold := calcDegreesThreshold(dc, 10)
 
-	// Count range of occurrences for each DiceValue;
 	// modifier -1 garantees that superior thresholds are exclusive when calculating fails and successes
-	criticalFailures = calculateRange(maxDiceValue, criticalFailureThreshold, modifier, 0)
-	failures = calculateRange(maxDiceValue, failureThreshold, modifier, -1) - criticalFailures
-	successes = calculateRange(maxDiceValue, successThreshold, modifier, -1) - failures - criticalFailures
+	criticalFailures = calcOddsRange(maxDiceValue, criticalFailureThreshold, modifier, 0)
+	failures = calcOddsRange(maxDiceValue, failureThreshold, modifier, -1) - criticalFailures
+	successes = calcOddsRange(maxDiceValue, successThreshold, modifier, -1) - failures - criticalFailures
 	criticalSuccesses = max(0, min(20, maxDiceValue-(successThreshold-modifier)))
 
 	// Check that success and criticalSuccesses are 0 when impossible
 	if modifier+maxDiceValue < failureThreshold {
-		//When highest value(20+modifier) < dc, we cant have success
+		//When highest dice value(20+modifier) < dc, we cant have success
 		successes = 0
 		criticalSuccesses = 0
 	}
 
 	naturalOne := minDiceValue + modifier
 	naturalTwenty := maxDiceValue + modifier
-	// Handle Natural One and Natural Twenty
+
 	HandleNaturalOne(naturalOne, criticalFailureThreshold, failureThreshold, successThreshold, maxDiceValue,
 		&criticalFailures, &failures, &successes, &criticalSuccesses)
 
